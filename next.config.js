e0bde7e6a0f78748686c4d58f840282ca0d5a9d1
Ryan Anthony <yanthony5204@gmail.com>
2025-11-18 20:47:31 +0700

    fix: remove unused config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    assetPrefix: isProd ? '/NextJS-Tailwind-App-Presentation-Page/' : '',
-    basePath: isProd ? '/NextJS-Tailwind-App-Presentation-Page' : '',
-    output: 'export',
-    images: {
-      unoptimized: true, // Disable default image optimization
-    },
 };
 
 module.exports = nextConfig;
