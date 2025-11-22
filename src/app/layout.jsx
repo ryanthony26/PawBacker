28a7065eeb2cf6f9f2785449e13ac9b35f3bbc4c
Ryan Anthony <yanthony5204@gmail.com>
2025-11-23 01:03:45 +0700

    fix: change web icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,12 +19,12 @@ export default function RootLayout({ children }) {
       <head>
         <link
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.3/css/all.min.css"
           integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/logos/logo.png" type="image/png" />
       </head>
 
       <body className={roboto.className}>
